807bb09101688e6497e4cf3bb16d4e4682eb380a
Rayal Khatri <kcrayalgw@gmail.com>
2022-11-14 15:00:07 +0545

    This is my new project

---
+++ java/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20)
+        {
+            $('.navbar').addClass("sticky");
+        }
+        else
+        {
+
+        }
+    })
+})
