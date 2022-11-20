dce99ad1ecda34c59eee3c0fa7f5888f9cdc91e7
Rayal Khatri <kcrayalgw@gmail.com>
2022-11-20 14:04:43 +0545

    Finisheed the passenger counting app Using Java

--- java/Java2.js
+++ java/Java2.js
@@ -2,10 +2,18 @@ count=0
 function incr()
 {
     count+=1
-    document.getElementById('Num').innerText = count
+    document.getElementById('Num').textContent = count
 }
 function decr()
 {
     count-=1
-    document.getElementById('Num').innerText = count
+    document.getElementById('Num').textContent= count
+}
+function save()
+{
+    document.getElementById('Count').textContent+=count
+    document.getElementById('Count').textContent+= " - " 
+    count = 0
+    document.getElementById('Num').textContent = count
+    
 }
