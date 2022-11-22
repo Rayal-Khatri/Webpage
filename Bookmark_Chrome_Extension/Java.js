b378f5c92fce0bbe56f3a1c8b693b8f184be85be
Rayal Khatri <kcrayalgw@gmail.com>
2022-11-22 11:59:19 +0545

    Bookmark App

---
+++ Bookmark_Chrome_Extension/Java.js
@@ -0,0 +1,8 @@
+let inputEl = document.getElementById('save-btn')
+let input = document.getElementById('input-content')
+
+links=[]
+inputEl.addEventListener("click",function()
+{
+    console.log(input.value)
+})
