525e592c405773046cb658ab922e39f1f3e9e25e
Rayal Khatri <kcrayalgw@gmail.com>
2022-11-22 21:07:34 +0545

    Bookmark App Progress

--- Bookmark_Chrome_Extension/Java.js
+++ Bookmark_Chrome_Extension/Java.js
@@ -1,8 +1,15 @@
 let inputEl = document.getElementById('save-btn')
 let input = document.getElementById('input-content')
-
+let valueEl = document.getElementById('links')
+let i=0
 links=[]
 inputEl.addEventListener("click",function()
 {
-    console.log(input.value)
+    links.push(input.value)
+    console.log(links)
+    // for(i=0;i<links.length;i++)
+    // {
+        valueEl.innerHTML += "<li>" + links[i] + "</li>"
+        i++    
+    // }
 })
