9e52d7628bf453ac3c0bd1a14f15e0bc718c2de0
Rayal Khatri <kcrayalgw@gmail.com>
2022-11-24 18:20:21 +0545

    Bookmark App Bug Fix

--- Bookmark_Chrome_Extension/Java.js
+++ Bookmark_Chrome_Extension/Java.js
@@ -1,15 +1,25 @@
 let inputEl = document.getElementById('save-btn')
 let input = document.getElementById('input-content')
 let valueEl = document.getElementById('links')
+let tempHTML =""
 let i=0
 links=[]
 inputEl.addEventListener("click",function()
 {
     links.push(input.value)
-    console.log(links)
-    // for(i=0;i<links.length;i++)
-    // {
-        valueEl.innerHTML += "<li>" + links[i] + "</li>"
-        i++    
-    // }
+    input.value=""
+    displaylink()
+    
 })
+function displaylink()
+{
+for(i=0;i<links.length;i++)
+tempHTML+=  `<li>
+                <a href="${links[i]}">
+                ${links[i]}
+                </a>
+            </li>
+            `
+valueEl.innerHTML = tempHTML  
+tempHTML=""
+}
