4d373f3dcaaf91bbb3bd6a1bb09e438e5fd46e37
Rayal Khatri <kcrayalgw@gmail.com>
2022-11-24 20:50:16 +0545

    Add Button to Remove the links

--- Bookmark_Chrome_Extension/Java.js
+++ Bookmark_Chrome_Extension/Java.js
@@ -1,25 +1,36 @@
 let inputEl = document.getElementById('save-btn')
+let clearEl = document.getElementById('clear-btn')
 let input = document.getElementById('input-content')
 let valueEl = document.getElementById('links')
 let tempHTML =""
 let i=0
 links=[]
+displaylink()
 inputEl.addEventListener("click",function()
 {
     links.push(input.value)
+    localStorage.setItem("myLinks",JSON.stringify(links))
     input.value=""
-    displaylink()
-    
+    displaylink()   
 })
 function displaylink()
 {
-for(i=0;i<links.length;i++)
-tempHTML+=  `<li>
-                <a href="${links[i]}">
-                ${links[i]}
-                </a>
-            </li>
-            `
-valueEl.innerHTML = tempHTML  
-tempHTML=""
+    
+    valueEl.innerHTML = ""  
+    tempstr=JSON.parse(localStorage.getItem("myLinks"))
+    for(i=0;i<tempstr.length;i++)
+    tempHTML+=  `<li>
+                    <a href="${tempstr[i]}">
+                    ${tempstr[i]}
+                    </a>
+                </li>
+                `
+    valueEl.innerHTML = tempHTML  
+    tempHTML=""
 }
+clearEl.addEventListener("click",function()
+{
+    links=[]
+    localStorage.clear()
+    displaylink()
+})
